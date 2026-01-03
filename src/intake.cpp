dd7c5205873cdb283ea5e5d768ed1d6d7810afe8
Sleepy <56746603+Jahan2cool@users.noreply.github.com>
2026-01-03 15:56:32 -0800

    fixed

--- src/intake.cpp
+++ src/intake.cpp
@@ -29,7 +29,7 @@ void Intake::update() {
                 counter = 0;
                 Task t{[&] {
                     bottom_intake.move(-127);
-                    delay(1000);
+                    delay(500);
 
                     std::cout << "[Intake] Anti-jam activated\n";
 
